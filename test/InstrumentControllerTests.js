03629e11df1ad6e88ed695ccfcafc51e3871e2a1
Dan Oved <stangogh@gmail.com>
2012-05-04 22:18:48 -0400

    created tests that define what happens when key on an instrument is pressed

---
+++ test/InstrumentControllerTests.js
@@ -0,0 +1,18 @@
+var assert = require('chai').assert,
+    all = require('node-promise').all,
+    Promise = require('node-promise').Promise,
+    sinon = require('sinon');
+
+describe('InstrumentController', function(){
+    describe('#constructor(broadcaster)', function(){
+        it('should keep copy of broadcaster', function(){
+
+        });
+    });
+
+    describe('#pressKey(key, instrumentName)', function(){
+        it("should fire keyPressed(key) in room for instrumentName on broadcaster", function(){
+
+        });
+    });
+});
