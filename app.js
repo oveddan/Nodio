5fffb18a38d11999b2e63eccbe48453f91ed9a97
Dan Oved <stangogh@gmail.com>
2012-05-07 11:06:48 -0400

    Ran express setup, but fixed package.json to include dev dependencies

--- app.js
+++ app.js
@@ -4,20 +4,17 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , security = require('./lib/security');
+  , routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
 // Configuration
+
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.set('view options', { layout: false });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  security.initialize(app);
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -32,8 +29,8 @@ app.configure('production', function(){
 
 // Routes
 
+app.get('/', routes.index);
 
-
-app.listen(3000, '127.0.0.1', function(){
-    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
