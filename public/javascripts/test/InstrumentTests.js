ca02e9a4128d1cc8603956009cbff82b92d6cd5e
Dan Oved <stangogh@gmail.com>
2012-05-07 14:24:39 -0400

    Started building key tests.  Will change so that key fires event, and instrument will subscribe to that event

---
+++ public/javascripts/test/InstrumentTests.js
@@ -0,0 +1,77 @@
+(function(){
+    TestCase('InstrumentView', {
+        'test should be Backbone view' : function(){
+        }
+    });
+    TestCase('InstrumentView.initialize()', {
+        'test should bindInstrumentModel' : function(){
+        }
+    });
+    TestCase('InstrumentView.bindInstrumentModel()', {
+        "test should create key views from all elements in view with class='key'" : function(){
+
+        },
+        "test should create and contain instrument model containing all key models": function(){
+
+        },
+        "test should create instrument model with name from data('name') from view": function(){
+
+        }
+    });
+}());
+
+(function(){
+    TestCase('InstrumentModel', {
+        'test should contain keys' : function(){
+
+        }
+    });
+
+    TestCase('InstrumentModel.initialize()', {
+        'test should listenForKeyPresses()' : function(){
+        },
+        'test shouldListenToInstrument()' : function(){
+        }
+    });
+
+    TestCase('InstrumentModel.listenToInstrument()', {
+        'test should connect to socket.io' : function(){
+        },
+        'test should contain instance of socket' : function(){
+        },
+        'test should emit listenToInstrument with name of instrument' : function(){
+        },
+        "test should bind 'keyPressed' event on socket to playKey" : function(){
+        }
+    });
+
+    TestCase("InstrumentModel.playKey(name)", {
+        'test should find key with name and call play() on it' : function(){
+        }
+    });
+
+}());
+
+(function(){
+    TestCase('KeyView', {
+        'test should extend Backbone view': function(){
+        }
+    });
+
+    TestCase('KeyView.events', {
+       'test should call pressKey() when key button clicked' : function(){
+       }
+    });
+
+    TestCase('KeyView.pressKey()' ,{
+       'test should fire keyPressedEvent with key name on InstrumentView' : function(){
+
+       }
+    });
+
+    TestCase('KeyView.play()', {
+        'test should call play() on audio element of view' : function(){
+
+        }
+    });
+}());_
