388d3a14960b26be060a7e1a8c23746da493e039
Dan Oved <stangogh@gmail.com>
2012-05-07 18:58:10 -0400

    listens for event listenToInstrument, but scope is not correct.  need to figure out how to test and fix

--- test/LibraryTests.js
+++ test/LibraryTests.js
@@ -38,14 +38,13 @@ describe('InstrumentBroadcaster', function(){
 
             expect(this.broadCaster.ensureSocketListening.called).to.be.ok;
         });
-        it('should wait for socket.io connection', function(){
-
-        });
         it('should listen for event listenToInstrument on socket.io connection', function(){
             // test
             this.broadCaster.listenForRequestsToHearInstrument();
             // assert
-            this.spyIoListener.on.calledWith('connection', this.spyIoListener.onConnection);
+            expect(this.spyIoListener.on.called).to.be.true;
+            expect(this.spyIoListener.on.args[0][0]).to.equal('connection');
+            expect(this.spyIoListener.on.args[0][1]).to.equal(this.broadCaster.onConnection);
         });
         it('should have socket join room with name of instrument when listenToInstrument fired', function(){
 
@@ -56,29 +55,30 @@ describe('InstrumentBroadcaster', function(){
         beforeEach(function(){
             this.app = {a : 5};
             this.broadCaster = new InstrumentBroadcaster(this.app);
-            this.listenStub = sinon.stub(socketio, 'listen');
+            this.spyIoListener = {
+                on : sinon.stub()
+            };
+            this.listenStub = sinon.stub(socketio, 'listen')
+                .returns(this.spyIoListener);
         });
         afterEach(function(){
             socketio.listen.restore();
         });
-        it('should load and listen to socket if socket is null', function(){
+        it('should load and listen to listener if listener is null', function(){
             // setup
             this.broadCaster.ioListener = null;
-            var ioListener = {b : 6};
-
-            this.listenStub.withArgs(this.app).returns(ioListener);
 
             // test
             this.broadCaster.ensureSocketListening();
             // assert
             expect(this.listenStub.calledWith(this.app)).is.true;
-            expect(this.broadCaster.ioListener).to.equal(ioListener);
+            expect(this.broadCaster.ioListener).to.equal(this.listenStub.returnValues[0]);
         });
-        it('should preserve original socket if socket it not null', function(){
+        it('should preserve original listener if listener is not null', function(){
             // setup
-            var originalListener = this.broadCaster.ioListener  = {a : 5};
-            var newListener = {d: 7};
-            this.listenStub.withArgs(this.app).returns(newListener);
+            this.broadCaster.ensureSocketListening();
+            var originalListener = this.broadCaster.ioListener;
+            this.listenStub.withArgs(this.app).returns({on : function(){}});
 
             // test
             this.broadCaster.ensureSocketListening();
